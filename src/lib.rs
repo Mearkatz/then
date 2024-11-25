aefd9c37de2b09b7ec0423b674ef01a884ccee1d
Mearkatz <drquinsey@gmail.com>
2024-11-25 08:02:33 -0500

    fixed macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 #[macro_export]
 macro_rules! then {
     ($x: expr, $y: expr) => {{
-        if let Some(y) = $x {
-            $y
+        if $x {
+            Some($y)
         } else {
             None
         }
